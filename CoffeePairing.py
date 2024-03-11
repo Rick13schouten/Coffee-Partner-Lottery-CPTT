8de1800ba2ee6f8eebce95489ea4ff4d6e3d38e8
Aristos7 <a.bilinis-moraitis@students.uu.nl>
2024-03-11 18:08:48 +0200

    various fixes in code

--- CoffeePairing.py
+++ CoffeePairing.py
@@ -1,3 +1,7 @@
+## project2-group17
+
+
+############ imports section ###############
 import pandas as pd
 import csv
 import random
@@ -5,6 +9,8 @@ import copy
 import os
 import math #for calculating the max number of group combinations 
 
+
+###################  Load Files Section (requirments 1,2) ###############
 # path to the CSV files with participant data
 participants_csv = "Coffee Partner Lottery participants.csv"
 
@@ -13,173 +19,145 @@ header_name = "Your name:"
 header_email = "Your e-mail:"
 
 # path to TXT file that stores the pairings of this round
-new_pairs_txt = "Coffee Partner Lottery new pairs.txt"
+new_groups_txt = "Coffee Partner Lottery new groups.txt"
 
-# path to CSV file that stores the pairings of this round
-new_pairs_csv = "Coffee Partner Lottery new pairs.csv"
+# path to CSV file that stores the groups of this round
+new_groups_csv = "Coffee Partner Lottery new groups.csv"
 
-# path to CSV file that stores all pairings (to avoid repetition)
-all_pairs_csv = "Coffee Partner Lottery all pairs.csv"
-
-
-#######################  Conversation Starters Section  ###########################
-
-# # read conversation starters CSV file
-# conv_starters=pd.read_csv("Conversation Starters.csv")
-
-# # path to stored conversation starters
-# stored_starters = "Stored Starters.csv"
-# #create list from stored starters
-# stored_starters_list = pd.read_csv(stored_starters)["used starters"].tolist()
-
-# #function selection of new conversation starter        
-# def conv_starters_selection():
-#   while True:
-#     random_index = random.randint(0, len(conv_starters) - 1)
-#     random_starter = conv_starters.iloc[random_index]
-#     # Check if all starters have been used
-#     if len(stored_starters_list) == len(conv_starters):
-#         stored_starters.clear()
-#         break
-#     # Check if the selected starter is already in stored_starters
-#     if random_starter.values[0] not in stored_starters_list:
-#         # Add the selected starter to stored_starters
-#         stored_starters_list.append(random_starter.values[0])
-#         #list stored to the CSV file again
-#         pd.DataFrame({"used starters": stored_starters_list}).to_csv(stored_starters, header=True, index=False)
-#         break  # Exit the loop if a unique starter is found
-#   return random_starter
-
-# # find conversation starter
-# con_starter = conv_starters_selection()
-# print(con_starter)
+# path to CSV file that stores all gropus (to avoid repetition)
+all_groups_csv = "Coffee Partner Lottery all groups.csv"
 
 
-# init set of old pairs
-opairs = set()
+############################## Group selection (requirements 3,4,5,6) ##########################
+
+# initial set of old groups 
+ogroups = set()
 
 DELIMITER=','
 
-# load all previous pairings (to avoid redundancies)
-if os.path.exists(all_pairs_csv):
-    with open(all_pairs_csv, "r") as file:
+# load all previous groups (to avoid redundancies)
+if os.path.exists(all_groups_csv):
+    with open(all_groups_csv, "r") as file:
         csvreader = csv.reader(file, delimiter=DELIMITER)
         for row in csvreader:
             group = []
             for i in range(0,len(row)):
                 group.append(row[i])                        
-            opairs.add(tuple(group))
+            ogroups.add(tuple(group))
 
+#create a participants list from the csv file
+participants_list = pd.read_csv(participants_csv)["Your name:"].tolist()
 
+# Calculate the number of participants
+num_participants = len(participants_list)
 
+# Randomly determine the number of groups
+num_groups = random.randint(1, num_participants // 2)
 
-### CODE ANNA
+# Shuffle the list of participants
+random.shuffle(participants_list)
 
-#import packages
-# import random
+#function to generate max possible combinations of groups according to the participants number
+def total_combinations(N):
+    total = 0
+    for k in range(2, N + 1):
+        total += math.comb(N, k)   # sum of combinations  of N objects taken k at a time 
+    return total
 
-# print("Welcome to the group randomizer! All people that signed up via the online form will be assigned to different groups of a random size. ")
+# Initialize a set to store the new groups
+new_groups = set()
 
+# Tries to generate new groups
+tries=0            #initialize number of tries to find new groups
+tries_limit= total_combinations(num_participants)    #limit of tries before decide all groups have been created
 
-# participants_list = ["Anna", "Joren", "Maud", "Merel", "Sofia", "Berend", "Willem", "Jelmer", "Pieter", "Emma", "Jorien"]
-
-
-# #calculate amount of participants based on the length of the participants lists from the form
-# amount_of_participants = len(participants_list)
-
-# #create random number of groups
-# number_of_groups = random.randint(1, amount_of_participants//2)
-
-
-# #create function that assigns people from list into random pairs or groups
-# def random_pairing(participants, number_of_groups):
+# Assign people from the list into random pairs or groups
+while True:
+    # Clear the new_groups set
+    new_groups.clear()
     
-#     #shuffle participants in list
-#     random.shuffle(participants)
-    
-#     #make random groups from shuffled participants 
-#    assigned_groups = []                                #list of groups
-#     for index in range(number_of_groups):                                             
-#         group = participants[index::number_of_groups]
-#         assigned_groups.append(group)                   #change assigned_groups variable with new made group
-    
-#     display groups
-#     for index, group in enumerate(assigned_groups):                           
-#         print(f"Group {index + 1}: {', '.join(group)}")
-        
+    # Make random groups from shuffled participants
+    for index in range(num_groups):
+        group = participants_list[index::num_groups]
+        new_groups.add(tuple(group))
 
-# randomly assign the participants to groups with an indicated groupsize
-# random_pairing(participants_list, number_of_groups)
-    
+    # Check if all new groups are indeed new, else reset
+    new_groups_found = True
+    for new_group in new_groups:
+      if new_group in ogroups:
+        new_groups_found = False
+        break
+    if new_groups_found:
+      break
+    else:
+      tries+=1
+      if tries>=tries_limit:
+          ogroups.clear()        
+          print("No new groups found. Participants may have been assigned to the same groups")
+          open(all_groups_csv, 'w').close()   # Clear the contents of the all_groups_csv file
+          break
+      random.shuffle(participants_list)
 
-# ###
+# Display groups
+for index, group in enumerate(new_groups):
+   print(f"Group {index + 1}: {', '.join(group)}")
+   
+# append groups to history file
+if os.path.exists(all_groups_csv):
+    mode = "a"
+else:
+    mode = "w"
+
+with open(all_groups_csv, mode) as file:
+    for group in new_groups:
+        group = list(group)
+        for i in range(0,len(group)):
+            if i < len(group)-1:
+                file.write(group[i] + DELIMITER)
+            else:
+                file.write(group[i] + "\n")
 
 
 
+#######################  Conversation Starters Section (requirement 7)  ###########################
+
+# # read conversation starters CSV file
+conv_starters=pd.read_csv("Conversation Starters.csv")
+
+# path to stored conversation starters
+stored_starters = "Stored Starters.csv"
+#create list from stored starters
+stored_starters_list = pd.read_csv(stored_starters)["used starters"].tolist()
+header_starters="used starters"
+
+#function selection of new conversation starter        
+def conv_starters_selection():
+  while True:
+    random_index = random.randint(0, len(conv_starters) - 1)
+    random_starter = conv_starters.iloc[random_index]
+    # Check if all starters have been used
+    if len(stored_starters_list) == len(conv_starters):
+        # Open the CSV file in write mode to clear its contents
+        with open(stored_starters, 'w') as file:
+              file.write(header_starters)          
+        break
+    # Check if the selected starter is already in stored_starters
+    if random_starter.values[0] not in stored_starters_list:
+        # Add the selected starter to stored_starters
+        stored_starters_list.append(random_starter.values[0])
+        #list stored to the CSV file again
+        pd.DataFrame({"used starters": stored_starters_list}).to_csv(stored_starters, header=True, index=False)
+        break  # Exit the loop if a unique starter is found
+  return random_starter
+
+# find conversation starter
+con_starter = conv_starters_selection()
+print(con_starter)
+
+
+#########################  Generate messages to groups (requirement 8) ############################# 
 # load participant's data
 formdata = pd.read_csv(participants_csv, sep=DELIMITER)
-
-# create duplicate-free list of participants
-participants = list(set(formdata[header_email]))
-
- # init set of new pairs
-npairs = set()
-
-# running set of participants
-nparticipants = copy.deepcopy(participants)
-
-# Boolean flag to check if new pairing has been found
-new_pairs_found = False
-
-# try creating new pairing until successful
-while not new_pairs_found:   # to do: add a maximum number of tries
-  
-    # if odd number of participants, create one triple, then pairs
-    if len(participants)%2 != 0:
-        
-        # take three random participants from list of participants
-        p1 = random.choice(nparticipants)
-        nparticipants.remove(p1)
-    
-        p2 = random.choice(nparticipants)
-        nparticipants.remove(p2)
-        
-        p3 = random.choice(nparticipants)
-        nparticipants.remove(p3)
-        
-        # create alphabetically sorted list of participants
-        plist = [p1, p2, p3]
-        plist.sort()
-                        
-        # add alphabetically sorted list to set of pairs
-        npairs.add(tuple(plist))
-
-  
-    # while still participants left to pair...
-    while len(nparticipants) > 0:
-
-        # take two random participants from list of participants
-        p1 = random.choice(nparticipants)
-        nparticipants.remove(p1)
-    
-        p2 = random.choice(nparticipants)
-        nparticipants.remove(p2)
-                
-        # create alphabetically sorted list of participants
-        plist = [p1, p2]
-        plist.sort()
-                        
-        # add alphabetically sorted list to set of pairs
-        npairs.add(tuple(plist))
-
- 
-    # check if all new pairs are indeed new, else reset
-    if npairs.isdisjoint(opairs):
-        new_pairs_found = True
-    else:
-        npairs = set()
-        nparticipants = copy.deepcopy(participants)
-
 
 # assemble output for printout
 output_string = ""
@@ -188,50 +166,39 @@ output_string += "------------------------\n"
 output_string += "Today's coffee partners:\n"
 output_string += "------------------------\n"
 
-for pair in npairs:
-    pair = list(pair)
+for group in new_groups:
     output_string += "* "
-    for i in range(0,len(pair)):
-        name_email_pair = f"{formdata[formdata[header_email] == pair[i]].iloc[0][header_name]} ({pair[i]})"
-        if i < len(pair)-1:
-            output_string += name_email_pair + ", "
-        else:
-            output_string += name_email_pair + "\n"
-    
+    for name in group:
+        # Find the corresponding email address in formdata based on the name
+        email = formdata.loc[formdata[header_name] == name, header_email].iloc[0]
+        # Append name and email pair to the output string
+        output_string += f"{name} ({email})"
+        # Add comma if it's not the last member of the group
+        if name != group[-1]:
+            output_string += ", "
+    output_string += "\n"
 # write output to console
 print(output_string)
 
 # write output into text file for later use
-with open(new_pairs_txt, "wb") as file:
+with open(new_groups_txt, "wb") as file:
     file.write(output_string.encode("utf8"))
 
-# write new pairs into CSV file (for e.g. use in MailMerge)
-with open(new_pairs_csv, "w") as file:
-    header = ["name1", "email1", "name2", "email2", "name3", "email3"]
-    file.write(DELIMITER.join(header) + "\n")
-    for pair in npairs:
-        pair = list(pair)
-        for i in range(0,len(pair)):
-            name_email_pair = f"{formdata[formdata[header_email] == pair[i]].iloc[0][header_name]}{DELIMITER} {pair[i]}"
-            if i < len(pair)-1:
-                file.write(name_email_pair + DELIMITER + " ")
-            else:
-                file.write(name_email_pair + "\n")
                 
-# append pairs to history file
-if os.path.exists(all_pairs_csv):
+# append groups to history file
+if os.path.exists(all_groups_csv):
     mode = "a"
 else:
     mode = "w"
 
-with open(all_pairs_csv, mode) as file:
-    for pair in npairs:
-        pair = list(pair)
-        for i in range(0,len(pair)):
-            if i < len(pair)-1:
-                file.write(pair[i] + DELIMITER)
+with open(all_groups_csv, mode) as file:
+    for group in new_groups:
+        group = list(group)
+        for i in range(0,len(group)):
+            if i < len(group)-1:
+                file.write(group[i] + DELIMITER)
             else:
-                file.write(pair[i] + "\n")
+                file.write(group[i] + "\n")
 
 
              
